90ed020c0010b99c6a15329e9ffba1b3aa575863
selim <selim-kose@live.com>
2025-12-17 15:00:07 +0100

    Remove slowmo and change default timeout

--- src/environment.py
+++ src/environment.py
@@ -3,11 +3,11 @@ from playwright.sync_api import sync_playwright
 def before_all(context):
     context.playwright = sync_playwright().start()
     context.browser_type = context.playwright.chromium
-    context.browser = context.browser_type.launch(headless=False, slow_mo=100)
+    context.browser = context.browser_type.launch(headless=False)
 
 def before_scenario(context, scenario):
     context.page = context.browser.new_page()
-    context.page.set_default_timeout(100)
+    context.page.set_default_timeout(200)
     context.base_url = "https://tap-vt25-testverktyg.github.io/exam--reading-list/"
 
 def after_scenario(context, scenario):
