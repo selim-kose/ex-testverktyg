a319993ed3df573b084d98d2b31db1cd6af2c4d5
selim <selim-kose@live.com>
2025-12-18 11:01:00 +0100

    Add headless mode

--- src/environment.py
+++ src/environment.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 def before_all(context):
     context.playwright = sync_playwright().start()
     context.browser_type = context.playwright.chromium
-    context.browser = context.browser_type.launch(headless=False)
+    context.browser = context.browser_type.launch(headless=True)
 
 def before_scenario(context, scenario):
     context.page = context.browser.new_page()
